8dacd6e1854f3d5b94bd82c2f23ab1b04309076d
Diego Delgado <diegodelgado365@gmail.com>
2023-10-15 20:45:19 -0300

    Agregado de certificado abreviado

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -1,51 +1,74 @@
 //Enviar los datos de la pagina principal a la del certificado
-const enviarDatos= ()=>{
-   let nombre= document.getElementById("nombre").value
-   let dni= document.getElementById("dni").value
-   let fecha= document.getElementById("fecha_bautismo").value
-   let libro= document.getElementById("libro").value
-   let pagina= document.getElementById("pagina").value
-   window.location.href = `./pages/certificado.html?nombre=${nombre}&dni=${dni}&fecha=${fecha}&libro=${libro}&pagina=${pagina}`
-}
-
+const enviarDatos = () => {
+  let nombre = document.getElementById("nombre").value;
+  let dni = document.getElementById("dni").value;
+  let fecha = document.getElementById("fecha_bautismo").value;
+  let libro = document.getElementById("libro").value;
+  let pagina = document.getElementById("pagina").value;
+  window.location.href = `./pages/certificado.html?nombre=${nombre}&dni=${dni}&fecha=${fecha}&libro=${libro}&pagina=${pagina}`;
+};
 
 function mostrarFormulario(tipo) {
-   Swal.fire({
-       title: 'Complete los siguientes campos:',
-       html:
-           '<br><label for="nombre">Nombre a quien va dirigido el certificado</label>' +
-           '<input id="nombre" class="swal2-input" placeholder="Nombre">' +
-           '<input type="number" id="dni" class="swal2-input" placeholder="DNI">' +
-           '<br><label for="fecha">Fecha de recibido el sacramento</label>' +
-           '<input type= "date" id="fecha" class="swal2-input" placeholder="Fecha">' +
-           '<br><label for="padres">Nombre de los padres (separados por "y")</label>' +
-           '<input id="padres" class="swal2-input" placeholder="Nombre Padres">' +
-           '<br><label for="padrinos">Nombre de los padrinos (separados por "y")</label>' +
-           '<input id="padrinos" class="swal2-input" placeholder="Nombre Padrinos">' +
-           '<label for="nroLibro">N° Libro:&nbsp </label>' +
-           '<input id="nroLibro" class="swal2-input" placeholder="Número de Libro"> <br>' +
-           '<label for="nroFolio">N° de folio:&nbsp </label>' +
-           '<input type="number" id="nroFolio" class="swal2-input" placeholder="Número de Folio">',
-       focusConfirm: false,
-       preConfirm: () => {
-           const nombre = Swal.getPopup().querySelector('#nombre').value;
-           const dni = Swal.getPopup().querySelector('#dni').value;
-           const fecha = Swal.getPopup().querySelector('#fecha').value;
-           const padres = Swal.getPopup().querySelector('#padres').value;
-           const padrinos = Swal.getPopup().querySelector('#padrinos').value;
-           const nroLibro = Swal.getPopup().querySelector('#nroLibro').value;
-           const nroFolio = Swal.getPopup().querySelector('#nroFolio').value;
+  Swal.fire({
+    title: "Complete los siguientes campos:",
+    html:
+      '<br><label for="nombre">Nombre a quien va dirigido el certificado</label>' +
+      '<input id="nombre" class="swal2-input" placeholder="Nombre">' +
+      '<input type="number" id="dni" class="swal2-input" placeholder="DNI">' +
+      '<br><label for="fecha">Fecha de recibido el sacramento</label>' +
+      '<input type= "date" id="fecha" class="swal2-input" placeholder="Fecha">' +
+      '<br><label for="padres">Nombre de los padres (separados por "y")</label>' +
+      '<input id="padres" class="swal2-input" placeholder="Nombre Padres">' +
+      '<br><label for="padrinos">Nombre de los padrinos (separados por "y")</label>' +
+      '<input id="padrinos" class="swal2-input" placeholder="Nombre Padrinos">' +
+      '<label for="nroLibro">N° Libro:&nbsp </label>' +
+      '<input id="nroLibro" class="swal2-input" placeholder="Número de Libro"> <br>' +
+      '<label for="nroFolio">N° de folio:&nbsp </label>' +
+      '<input type="number" id="nroFolio" class="swal2-input" placeholder="Número de Folio">',
+    focusConfirm: false,
+    preConfirm: () => {
+      const nombre = Swal.getPopup().querySelector("#nombre").value;
+      const dni = Swal.getPopup().querySelector("#dni").value;
+      const fecha = Swal.getPopup().querySelector("#fecha").value;
+      const padres = Swal.getPopup().querySelector("#padres").value;
+      const padrinos = Swal.getPopup().querySelector("#padrinos").value;
+      const nroLibro = Swal.getPopup().querySelector("#nroLibro").value;
+      const nroFolio = Swal.getPopup().querySelector("#nroFolio").value;
 
-           // Puedes hacer lo que desees con los datos aquí, como enviarlos a un servidor o mostrarlos en la consola.
-           console.log('Nombre:', nombre);
-           console.log('DNI:', dni);
-           console.log('Fecha:', fecha);
-           console.log('Nombre de Padrinos: ', padrinos);
-           console.log('Número de Libro:', nroLibro);
-           console.log('Número de Folio:', nroFolio);
-           window.location.href = `./pages/certificado.html?nombre=${nombre}&dni=${dni}&fecha=${fecha}&libro=${nroLibro}&folio=${nroFolio}&padrinos=${padrinos}&tipo=${tipo}&padres=${padres}`
-       }
-   });
+      // Puedes hacer lo que desees con los datos aquí, como enviarlos a un servidor o mostrarlos en la consola.
+      console.log("Nombre:", nombre);
+      console.log("DNI:", dni);
+      console.log("Fecha:", fecha);
+      console.log("Nombre de Padrinos: ", padrinos);
+      console.log("Número de Libro:", nroLibro);
+      console.log("Número de Folio:", nroFolio);
+      window.location.href = `./pages/certificado.html?nombre=${nombre}&dni=${dni}&fecha=${fecha}&libro=${nroLibro}&folio=${nroFolio}&padrinos=${padrinos}&tipo=${tipo}&padres=${padres}`;
+    },
+  });
 }
 
-         
+
+function mostrarFormularioAbreviado(tipo) {
+  Swal.fire({
+    title: "Complete los siguientes campos:",
+    html:
+      '<br><label for="nombre">Nombre a quien va dirigido el certificado</label>' +
+      '<input id="nombre" class="swal2-input" placeholder="Nombre">' +
+      '<input type="number" id="dni" class="swal2-input" placeholder="DNI">' +
+      '<br><label for="fecha">Fecha de recibido el sacramento</label>' +
+      '<input type= "date" id="fecha" class="swal2-input" placeholder="Fecha">' +
+      '<label for="nroLibro">N° Libro:&nbsp </label>' +
+      '<input id="nroLibro" class="swal2-input" placeholder="Número de Libro"> <br>' +
+      '<label for="nroFolio">N° de folio:&nbsp </label>' +
+      '<input type="number" id="nroFolio" class="swal2-input" placeholder="Número de Folio">',
+    focusConfirm: false,
+    preConfirm: () => {
+      const nombre = Swal.getPopup().querySelector("#nombre").value;
+      const dni = Swal.getPopup().querySelector("#dni").value;
+      const fecha = Swal.getPopup().querySelector("#fecha").value;
+      const nroLibro = Swal.getPopup().querySelector("#nroLibro").value;
+      const nroFolio = Swal.getPopup().querySelector("#nroFolio").value;
+      window.location.href = `./pages/certificadoAbreviado.html?nombre=${nombre}&dni=${dni}&fecha=${fecha}&libro=${nroLibro}&folio=${nroFolio}&tipo=${tipo}`;
+    },
+  });
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,122 @@
-
-
-
 //Descarga la imagen de la página
-imprimirCertificado=()=>{
-    Swal.fire({
-        title: 'Nombre del archivo',
-        input: 'text',
-        inputPlaceholder: 'Escribe un nombre para el archivo',
-        showCancelButton: true,
-        confirmButtonText: 'Descargar',
-        cancelButtonText: 'Cancelar',
-        inputValidator: (value) => {
-            if (!value) {
-                return 'Debes ingresar un nombre para el archivo';
-            }
-        }
-    }).then((result) => {
-        if (result.isConfirmed) {
-            const container = document.getElementById('container');
-            html2canvas(container).then(function(canvas) {
-                const imgData = canvas.toDataURL('image/jpeg');
-                const a = document.createElement('a');
-                a.href = imgData;
-                a.download = result.value + '.jpeg'; 
-                a.click();
-            });
-        }
-    });
-}
+imprimirCertificado = () => {
+  Swal.fire({
+    title: "Nombre del archivo",
+    input: "text",
+    inputPlaceholder: "Escribe un nombre para el archivo",
+    showCancelButton: true,
+    confirmButtonText: "Descargar",
+    cancelButtonText: "Cancelar",
+    inputValidator: (value) => {
+      if (!value) {
+        return "Debes ingresar un nombre para el archivo";
+      }
+    },
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const container = document.getElementById("container");
+      html2canvas(container).then(function (canvas) {
+        const imgData = canvas.toDataURL("image/jpeg");
+        const a = document.createElement("a");
+        a.href = imgData;
+        a.download = result.value + ".jpeg";
+        a.click();
+      });
+    }
+  });
+};
 
-const imprimirCert =(nombre)=>
-{
-    
-    const container = document.getElementById('container');
-            html2canvas(container).then(function(canvas) {
-                const imgData = canvas.toDataURL('image/jpeg');
-                const a = document.createElement('a');
-                a.href = imgData;
-                a.download = nombre + '.jpeg'; 
-                a.click();
-            });
-            
-
-        
-        }
-
-
+const imprimirCert = (nombre) => {
+  const container = document.getElementById("container");
+  html2canvas(container).then(function (canvas) {
+    const imgData = canvas.toDataURL("image/jpeg");
+    const a = document.createElement("a");
+    a.href = imgData;
+    a.download = nombre + ".jpeg";
+    a.click();
+  });
+};
 
 //Tomar los datos enviados desde la página principal
 
+const tomarDatosCertificado = () => {
+  const hoy = new Date();
+  const meses = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre",
+  ];
 
-const tomarDatosCertificado=()=>{
-    const hoy= new Date()
-    const meses = [
-        "enero", "febrero", "marzo", "abril", "mayo", "junio",
-        "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
-    ];
-
-  const fechaCertificado= document.getElementById("fechaCertificado")
+  const fechaCertificado = document.getElementById("fechaCertificado");
 
   const url = new URL(window.location.href);
-  let tipo= url.searchParams.get("tipo");
-  let articulo
+  let tipo = url.searchParams.get("tipo");
+  let articulo;
   let nombre = url.searchParams.get("nombre");
   let dni = url.searchParams.get("dni");
-  let fecha = new Date(url.searchParams.get("fecha"))
+  let fecha = new Date(url.searchParams.get("fecha"));
   let libro = url.searchParams.get("libro");
   let folio = url.searchParams.get("folio");
   let padrinos = url.searchParams.get("padrinos");
   let padres = url.searchParams.get("padres");
-  const textoPrincipal= document.getElementById("textoPrincipal")
-  fechaCertificado.innerHTML=`Córdoba, ${hoy.getDate()} de ${meses[hoy.getMonth()]} de ${hoy.getFullYear()}` 
-  const titulo= document.getElementById("titulo")
-  textoPrincipal.innerHTML=`Por la presente certifico que <b>${nombre.toUpperCase()}</b>, DNI ${dni} recibió el sacramento de ${tipo} en esta parroquia, Nuestra Señora de los Ángeles, el día <b> ${fecha.getDate()} de ${meses[fecha.getMonth()]} de ${fecha.getFullYear()}</b> 
- , sus padres son: ${padres}. Sus padrinos son: ${padrinos}.<br>Según consta en el libro n° ${libro}, folio ${folio}`
- titulo.innerHTML=titulo.innerHTML +tipo.toLocaleUpperCase()
- imprimirCert(nombre)
- 
- 
-  
-}
+  const textoPrincipal = document.getElementById("textoPrincipal");
+  fechaCertificado.innerHTML = `Córdoba, ${hoy.getDate()} de ${
+    meses[hoy.getMonth()]
+  } de ${hoy.getFullYear()}`;
+  const titulo = document.getElementById("titulo");
+  textoPrincipal.innerHTML = `Por la presente certifico que <b>${nombre.toUpperCase()}</b>, DNI ${dni} recibió el sacramento de ${tipo} en esta parroquia, Nuestra Señora de los Ángeles, el día <b> ${fecha.getDate()} de ${
+    meses[fecha.getMonth()]
+  } de ${fecha.getFullYear()}</b> 
+ , sus padres son: ${padres}. Sus padrinos son: ${padrinos}.<br>Según consta en el libro n° ${libro}, folio ${folio}`;
+ titulo.innerHTML = titulo.innerHTML+" " + tipo.toLocaleUpperCase();
+  imprimirCert(nombre);
+};
+
+const tomarDatosCertificadoAbreviado = () => {
+  const hoy = new Date();
+  const meses = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre",
+  ];
+
+  const fechaCertificado = document.getElementById("fechaCertificado");
+
+  const url = new URL(window.location.href);
+  let tipo = url.searchParams.get("tipo");
+  let articulo;
+  let nombre = url.searchParams.get("nombre");
+  let dni = url.searchParams.get("dni");
+  let fecha = new Date(url.searchParams.get("fecha"));
+  let libro = url.searchParams.get("libro");
+  let folio = url.searchParams.get("folio");
+  const textoPrincipal = document.getElementById("textoPrincipal");
+  fechaCertificado.innerHTML = `Córdoba, ${hoy.getDate()} de ${
+    meses[hoy.getMonth()]
+  } de ${hoy.getFullYear()}`;
+  const titulo = document.getElementById("titulo");
+  textoPrincipal.innerHTML = `Por la presente certifico que <b>${nombre.toUpperCase()}</b>, DNI ${dni} recibió el sacramento del ${tipo} en esta parroquia, Nuestra Señora de los Ángeles, el día <b> ${fecha.getDate()} de ${
+    meses[fecha.getMonth()]
+  } de ${fecha.getFullYear()}</b> 
+  <br>Según consta en el libro n° ${libro}, folio ${folio}`;
+  titulo.innerHTML = titulo.innerHTML+" " + tipo.toLocaleUpperCase();
+  imprimirCert(nombre);
+};
